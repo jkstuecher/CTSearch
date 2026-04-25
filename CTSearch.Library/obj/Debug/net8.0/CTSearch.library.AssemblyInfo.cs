45e509fde9534c26b3b69c0f989aa41236c9e134
Jeff Stuecher <stuecher.1@osu.edu>
2026-04-25 10:45:17 -0400

    Struc changes

--- CTSearch.Library/obj/Debug/net8.0/CTSearch.library.AssemblyInfo.cs
+++ CTSearch.Library/obj/Debug/net8.0/CTSearch.library.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CTSearch.library")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+556fc718f71efec663d1343eb0289d7d8d43bf80")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ac65b53dfb2e43bd4d39f5befd0ab4a1d77a229")]
 [assembly: System.Reflection.AssemblyProductAttribute("CTSearch.library")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CTSearch.library")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
